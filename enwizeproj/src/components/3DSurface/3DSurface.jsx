9eacd7104bc6ddd2e2f4ae4a7ab52f9d83dfd9d2
Adi Vaknin <arbiva0@gmail.com>
2023-07-11 16:01:35 +0300

    first comm

---
+++ enwizeproj/src/components/3DSurface/3DSurface.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThreeDSurface = () => {
+  return (
+    <div>3DSurface</div>
+  )
+}
+
+export default ThreeDSurface
---
+++ enwizeproj/src/components/MainView/MaiView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MaiView = () => {
+  return (
+    <div>MaiView</div>
+  )
+}
+
+export default MaiView
